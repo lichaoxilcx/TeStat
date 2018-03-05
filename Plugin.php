b5b0325952b4f3ecead7f434237b9d091370925d
LiCxi <2992368059@qq.com>
2018-03-05 21:38:14 +0800

    重新实现‘ ‘获取文章是否已点赞’的功能，简化代码

--- Plugin.php
+++ Plugin.php
@@ -77,85 +77,21 @@ class TeStat_Plugin implements Typecho_Plugin_Interface
 		$delFields = new Typecho_Widget_Helper_Form_Element_Radio('delFields',
             array(0=>_t('保留数据'),1=>_t('删除数据'),), '0', _t('卸载设置'),_t('卸载插件后数据是否保留'));
         $form->addInput($delFields);
-		$allow_stat = new Typecho_Widget_Helper_Form_Element_Radio('allow_stat',
-            array(0=>_t('关闭'),1=>_t('开启'),), '1', _t('统计运行信息'),_t('是否开启运行信息统计'));
+        $allow_stat = new Typecho_Widget_Helper_Form_Element_Radio('allow_stat',
+            array(0=>_t('关闭'),1=>_t('开启'),), '0', _t('统计运行信息'),_t('是否开启运行信息统计'));
         $form->addInput($allow_stat);
-		$allow_stat_mem = new Typecho_Widget_Helper_Form_Element_Radio('allow_stat_mem',
-            array(0=>_t('关闭'),1=>_t('开启'),), '1', _t('统计内存开销'),_t('是否开启内存开销统计'));
+        $allow_stat_mem = new Typecho_Widget_Helper_Form_Element_Radio('allow_stat_mem',
+            array(0=>_t('关闭'),1=>_t('开启'),), '0', _t('统计内存开销'),_t('是否开启内存开销统计'));
         $form->addInput($allow_stat_mem);
 
         /*
         @author LiCxi
          */
-        $style_csscode = new Typecho_Widget_Helper_Form_Element_Textarea('style_csscode', null,
-        	".testat-dialog{
-				position:fixed;
-				bottom:40px;
-				right:100px;
-				padding:10px;background-color:#fff;
-				display:none;
-				-webkit-border-radius: 3px;
-				-moz-border-radius: 3px;
-				border-radius: 3px;
-				z-index:1024;
-			}
-			.testat-dialog.error{
-				background-color:#f40;
-				color:#fff;
-			}
-			.testat-dialog.success{
-				background-color:#24AA42;
-				color:#fff;
-			}",
-        	_t('自定义CSS样式'), _t('此部分需要有部分CSS基础，当前CSS适配<a href="https://github.com/lichaoxilcx/typecho-theme-material">Material</a>的主题'));
-        $form->addInput($style_csscode);
 
-		$like_btn_click_jscode = new Typecho_Widget_Helper_Form_Element_Textarea('like_btn_click_jscode', null,
-			"$(function(){
-	$('.btn-like').click(function(e){ //按钮element
-		e.stopPropagation(); //勿删
-		e.preventDefault(); //勿删
-		var that = $(this),num = $(this).data('num'), cid = $(this).data('cid'); //获取数据
-		if(cid===undefined) return false;
-		$.get(window.action+'likes?cid='+cid).success(function(rs){
-			if(rs.status==1){
-				//====点赞成功====//
-				$('.btn-like i').attr('data-badge', num+1);
-				$('.btn-like i').css('color', 'red');
-				testatAlert(rs.msg===undefined ? '已成功为该文章点赞!' : rs.msg);
-				//====点赞成功====//
-			}else{
-				//====点赞成功====//
-				testatAlert(rs.msg===undefined ? '操作出错!' : rs.msg,'err');
-				//====点赞成功====//
-			}
-		});
-	});
-});
-//提示框
-function testatAlert(msg,type,time){
-	type = type === undefined ? 'success' : 'error';
-	time = time === undefined ? (type=='success' ? 1500 : 3000) : time;
-	var html = '<div class=\"testat-dialog '+type+'\">'+msg+'</div>';
-	$(html).appendTo($('body')).fadeIn(300,function(){
-		setTimeout(function(){
-			$('body > .testat-dialog').remove();
-		},time);
-	});
-}",
-			_t('点赞按钮点击事件JS代码'), _t('此部分需要有部分javascript或者jquery基础，主要修改注释部分，当前JS适配<a href="https://github.com/lichaoxilcx/typecho-theme-material">Material</a>的主题'));
-    	$form->addInput($like_btn_click_jscode);
-
-    	$test_client_is_like_article_jscode = new Typecho_Widget_Helper_Form_Element_Textarea('test_client_is_like_article_jscode', null,
-    		"$.get(window.action+'likes?cid='+$('.btn-like').data('cid')+'&type=islike').success(function(rs){
-	if(rs.status == 1){
-		$('.btn-like i').css('color', 'red');
-	} else {
-	}
-
-});",
-    		 _t('打开页面时检测浏览器是否对文章已点赞JS代码'), _t('此部分需要有部分javascript或者jquery基础，主要修改注释部分，当前JS适配<a href="https://github.com/lichaoxilcx/typecho-theme-material">Material</a>的主题'));
-    	$form->addInput($test_client_is_like_article_jscode);
+        $like_btn_elem = new Typecho_Widget_Helper_Form_Element_Text('like_btn_elem', null, '#like_btn', _t('点赞按钮元素 *'), _t('请确保该元素唯一，多元素操作请前往Plugin.php修改代码或者期待下一个版本'));
+		$form->addInput($like_btn_elem);
+        $like_num_elem = new Typecho_Widget_Helper_Form_Element_Text('like_num_elem', null, '#like_num', _t('点赞数目元素 *'), _t('此元素用于点赞后更新点赞数'));
+		$form->addInput($like_num_elem);
 	}
 
     /**
@@ -178,6 +114,7 @@ function testatAlert(msg,type,time){
 		if($options->allow_stat_mem)
 			self::$mem['begin'] =  memory_get_usage();
 	}
+
 	//记录执行结束的参数
 	public static function setEnd(){
 		$options = Typecho_Widget::widget('Widget_Options')->plugin('TeStat');
@@ -192,6 +129,7 @@ function testatAlert(msg,type,time){
 			self::$mem['end'] =  memory_get_usage();
 
 	}
+
 	//显示记录的各种统计项
 	public static function runtime($format='吞吐率:{rate},运行时间:{runtime},内存开销:{mem},加载文件:{files}'){
 		$options = Typecho_Widget::widget('Widget_Options');
@@ -219,6 +157,7 @@ function testatAlert(msg,type,time){
 		}
 		echo str_replace(array('{runtime}','{mem}','{files}','{rate}'),$stat,$format);
 	}
+
     /**
      * 增加浏览量
      * @params Widget_Archive   $archive
@@ -235,27 +174,90 @@ function testatAlert(msg,type,time){
             }
             if(!in_array($cid,$views)){
                 $db = Typecho_Db::get();
-                $db->query($db->update('table.contents')->rows(array('viewsNum' => (int)$archive->viewsNum+1))->where('cid = ?', $cid));
+                $db->query($db->update('table.contents')->rows(array('viewsNum' => (int)$archive->viewsNum + 1))->where('cid = ?', $cid));
+                $archive->viewsNum = $archive->viewsNum + 1;
                 array_push($views, $cid);
                 $views = implode(',', $views);
-                Typecho_Cookie::set('__post_views', $views); //记录查看cookie
+                Typecho_Cookie::set('__post_views', $views, time() + 300); //记录查看cookie
             }
         }
     }
+
+    public static function isliked($cid) {
+		$likes = Typecho_Cookie::get('__post_likes');
+		if(empty($likes)) {
+			$likes = array();
+		} else {
+			$likes = explode(',', $likes);
+		}
+		if(!in_array($cid, $likes)) {
+			return false;
+		} else {
+			return true;
+		}
+    }
+
 	public static function insertCss($header,$widget){
 
-		$options = Typecho_Widget::widget('Widget_Options')->plugin('TeStat');
-
 		$action = Typecho_Common::url('/action/',Helper::options()->index);
-		echo '<script type="text/javascript">window.action="'.$action.'";</script>'.'<style type="text/css">'.$options->style_csscode.'</style>';
+		echo '<style type="text/css">.testat-dialog{
+				position:fixed;
+				bottom:40px;
+				right:100px;
+				padding:10px;background-color:#fff;
+				display:none;
+				-webkit-border-radius: 3px;
+				-moz-border-radius: 3px;
+				border-radius: 3px;
+				z-index:1024;
+			}
+			.testat-dialog.error{
+				background-color:#f40;
+				color:#fff;
+			}
+			.testat-dialog.success{
+				background-color:#24AA42;
+				color:#fff;
+			}</style><script type="text/javascript">window.action="'.$action.'";</script>';
 	}
+
 	public static function insertJs($widget){
 
 		$options = Typecho_Widget::widget('Widget_Options')->plugin('TeStat');
 
-		$script = $options->test_client_is_like_article_jscode . $options->like_btn_click_jscode;
-		echo '<script type="text/javascript">'.$script.'</script>';
+		$script = "<script type=\"text/javascript\">
+$(function(){
+	$('". $options->like_btn_elem ."').click(function(e){
+		e.stopPropagation();
+		e.preventDefault();
+		var that = $(this),num = $(this).data('num'), cid = $(this).data('cid');
+		if(cid===undefined) return false;
+		$.get(window.action+'likes?cid='+cid).success(function(rs){
+			if(rs.status==1){
+				//兼容Matrial主题
+				$('".$options->like_num_elem."').attr('data-badge', num+1);
+				$('".$options->like_num_elem."').css('color', 'red');
+				testatAlert(rs.msg===undefined ? '已成功为该文章点赞!' : rs.msg);
+			}else{
+				testatAlert(rs.msg===undefined ? '操作出错!' : rs.msg,'err');
+			}
+		});
+	});
+});
+function testatAlert(msg,type,time){
+	type = type === undefined ? 'success' : 'error';
+	time = time === undefined ? (type=='success' ? 1500 : 3000) : time;
+	var html = '<div class=\"testat-dialog '+type+'\">'+msg+'</div>';
+	$(html).appendTo($('body')).fadeIn(300,function(){
+		setTimeout(function(){
+			$('body > .testat-dialog').remove();
+		},time);
+	});
+}
+</script>";
+		echo $script;
 	}
+
     //cleanAttribute('fields')清除查询字段，select *
     public static function selectHandle($archive){
         $user = Typecho_Widget::widget('Widget_User');
